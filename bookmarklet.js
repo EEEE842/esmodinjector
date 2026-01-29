a04b6035bfc065352a4af89d8499b1e37cb673c2
EEEE842 <noahwinstonage12edmonton104ave@atomicmail.io>
2026-01-29 08:10:23 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,16 @@
 javascript:(function(){
-  window.ES_SELECTED_SB3="https://raw.githubusercontent.com/EEEE842/esmodinjector/main/test.sb3";
-  var s=document.createElement("script");
-  s.src="https://raw.githubusercontent.com/EEEE842/esmodinjector/main/injector.js";
-  document.body.appendChild(s);
+function inject(win){
+  win.ES_SELECTED_SB3 = "https://raw.githubusercontent.com/EEEE842/esmodinjector/main/test.sb3";
+  var s = win.document.createElement("script");
+  s.src = "https://raw.githubusercontent.com/EEEE842/esmodinjector/main/injector.js";
+  win.document.body.appendChild(s);
+}
+
+if(location.hostname.includes("turbowarp.org")){
+  let f = document.querySelector("iframe");
+  if(!f){ alert("TurboWarp iframe not found"); return; }
+  inject(f.contentWindow);
+}else{
+  inject(window);
+}
 })();
