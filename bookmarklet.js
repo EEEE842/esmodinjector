62266113b22cf9c112a6c48f185f1a82c2dab1b0
EEEE842 <noahwinstonage12edmonton104ave@atomicmail.io>
2026-01-28 20:06:15 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,12 @@
 javascript:(function(){
-fetch("https://raw.githubusercontent.com/EEEE842/test/main/injector.js")
-  .then(r=>r.text())
-  .then(code=>eval(code));
+  alert("Loading injection...");
+  fetch("https://raw.githubusercontent.com/EEEE842/test/main/injector.js")
+    .then(r=>r.text())
+    .then(code=>{
+      eval(code);
+      alert("Injection complete!");
+    })
+    .catch(()=>{
+      alert("Injection failed!");
+    });
 })();
