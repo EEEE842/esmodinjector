00f5e924ee9c411ad06b59a7b6ea369c3064e4ae
EEEE842 <noahwinstonage12edmonton104ave@atomicmail.io>
2026-01-28 21:18:48 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function(){
   alert("Loading injection...");
-  fetch("https://raw.githubusercontent.com/EEEE842/namehereinjectablemenu/main/injector.js")
+  fetch("https://raw.githubusercontent.com/EEEE842/esmodinjector/main/injector.js")
     .then(r=>r.text())
     .then(code=>{
       eval(code);
